e4b51cbeb72e59b8108f5f42717838272ab20c12
Michael Burton <micbur1@gmail.com>
2021-01-30 16:08:12 -0800

    chancge the order of buisiness logic and user logic in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,36 @@
+let Numbers =[]
+//Business logic
+function beepBoop(userInput){
+  if (isNaN(userInput)) {
+    alert("enter a real number my dude!")
+    return;
+  }
+  for(let i=0; i<=userInput; i++){
+    let replace = i
+    if (i.toString().includes(1)){
+      replace="beep";      
+    }
+    if (i.toString().includes(2)){
+      replace="boop"; 
+    }
+    if (i.toString().includes(3)){
+      replace="Won't you be my neighbor?"; 
+    }
+    Numbers.push(replace); 
+  }
+  return Numbers;
+};
+
+function clearIt() {
+  var list = document.getElementById("output");
+  while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild);
+  }
+  Numbers = []
+};
+
+//user Logic
+
 $(document).ready(function(){
   $("#form1").submit(function(event){
 
@@ -10,38 +43,6 @@ $(document).ready(function(){
   });
  
 });
-let Numbers =[]
-function beepBoop(userInput){
-  if (isNaN(userInput)) {
-    alert("enter a real number my dude!")
-    console.log(userInput)
-    return;
-  }
-  for(let i=0; i<=userInput; i++){
-    let replace = i
-    if (i.toString().includes(1)){
-       replace="beep";      
-    }
-    if (i.toString().includes(2)){
-       replace="boop"; 
-    }
-    if (i.toString().includes(3)){
-      replace="Won't you be my neighbor?"; 
-    }
-    Numbers.push(replace); 
-  }
-  return Numbers
-}
-
-function clearIt() {
-  console.log("button clicked");
-  var list = document.getElementById("output");
-  while (list.hasChildNodes()) {
-    list.removeChild(list.firstChild);
-  }
-  Numbers = []
-};
-
 
 
 
